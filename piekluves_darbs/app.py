1b6dcce67f7f0133f9b0255478cf40012e531d25
liepinjjjsss <161810179+liepinjjjsss@users.noreply.github.com>
2025-02-17 10:02:59 +0200

    i hate html and css even more now

--- piekluves_darbs/app.py
+++ piekluves_darbs/app.py
@@ -29,6 +29,13 @@ app = Flask(__name__, static_folder="static", template_folder="templates")
 def index():
     return render_template("index.html")
 
+@app.route("/dashboard")
+def dashboard():
+    return render_template("dashboard.html")
+
+if __name__ == "__main__":
+    app.run(debug=True)
+
 
         
         
