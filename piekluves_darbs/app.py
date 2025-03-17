666ab6714b165a1b7f6786d13a32cac221cc18d3
liepinjjjsss <lieparenars6@gmail.com>
2025-03-17 10:18:51 +0200

    done some things

--- piekluves_darbs/app.py
+++ piekluves_darbs/app.py
@@ -100,10 +100,15 @@ def submit():
             curr.execute("""SELECT name FROM users WHERE email = ? AND password = ?""", (username, password))
             name  = curr.fetchone()
 
+            curr.execute("""SELECT id FROM users WHERE email = ? AND password = ?""", (username, password))
+            id = curr.fetchone()
+
+
             print(name)
-            print(name[0])
+            print(id)
 
             session["name"] = name[0]
+            session["user_id"] = id[0]
 
             conn.close()
 
@@ -121,12 +126,44 @@ def dashboard():
     name = session["name"]
     return render_template("dashboard.html", username = name)
 
+@app.route("/overview")
+def overview():
+    name = session["name"]
+    return render_template("overview.html", username = name)
+
 @app.route("/add_transaction", methods=['POST','GET'])
 def add_transaction():
     amount = request.form.get("amount")
+    reciever = request.form.get("reciever")
     print(amount)
+    print(reciever)
+
+    conn = sql.connect("expensify.db")
+    curr = conn.cursor()
+
+    curr.execute("""INSERT INTO transactions (user_id, amount, sender, reciever) VALUES (?,?,?,?)""", (session["user_id"], amount, session["name"], reciever))
+
+    conn.commit()
+    conn.close()
 
     return dashboard()
+
+@app.route("/add_business", methods=['POST', 'GET'])
+def add_business():
+    owner_id = session["user_id"]
+    business_name = request.form.get("business_name")
+    business_type = request.form.get("business_type")
+    net_worth = request.form.get("net_worth")
+
+    conn = sql.connect("expensify.db")
+    curr = conn.cursor()    
+
+    curr.execute("""INSERT INTO businesses (owner_id, business_name, business_type, net_worth) VALUES (?,?,?,?)""", (owner_id, business_name, business_type, net_worth))
+
+    conn.commit()
+    conn.close()
+
+    return overview()
 
 if __name__ == "__main__":
     app.run(debug=True)
